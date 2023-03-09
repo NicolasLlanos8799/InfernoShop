dfd439f102476d7274716ad539cbc47c1596bfe6
Nicolas Llanos <nllanos@applica.dev>
2023-03-09 16:30:28 -0300

    enviandole valor de cantidad

--- assets/js/funcionesParaLaOrden.js
+++ assets/js/funcionesParaLaOrden.js
@@ -2,15 +2,16 @@
 
 
 var carritoDeSemillas = obtenerCarrito();
-var currentValue = 1;
+var currentValue = 1; // Variable global
+
 function guardarValor(valorBoton) {
-  if (!carritoDeSemillas.includes(valorBoton)) {
+  // if (!carritoDeSemillas.includes(valorBoton)) {
     carritoDeSemillas.push(valorBoton);
     guardarCarrito(carritoDeSemillas);
-    console.log("Valor del botón: " + carritoDeSemillas + "Cantidad: " + currentValue);
-  } else {
-    console.log("El valor ya existe en el array: " + valorBoton);
-  }
+    console.log("Valor del botón: " + carritoDeSemillas + " cantidad de packs: " + currentValue);
+  // } else {
+  //   console.log("El valor ya existe en el array: " + valorBoton);
+  // }
 }
 
 function obtenerCarrito() {
@@ -31,7 +32,11 @@ function cambiarColor(idBoton) {
 }
 
 function enviarCarrito() {
-  var mensaje = "Mi pedido: " + carritoDeSemillas.join(", ");
+  var mensaje = "Mi pedido de semillas es: ";
+  for (var i = 0; i < carritoDeSemillas.length; i++) {
+    mensaje += carritoDeSemillas[i] + " x " + currentValue + ", ";
+  }
+  mensaje = mensaje.slice(0, -2); // Eliminar la última coma y espacio
   mensaje = mensaje.replace(/ /g, "%20");
   var enlace = "https://wa.me/543425087441/?text=" + mensaje;
   window.open(enlace);
@@ -43,37 +48,35 @@ function irAPagina(pagina) {
 
 window.addEventListener('load', function() {
   // Obtener todos los elementos con la clase "quantity-input"
-var quantityInputs = document.querySelectorAll(".quantity-input");
+  var quantityInputs = document.querySelectorAll(".quantity-input");
 
-// Iterar sobre los elementos y agregar los eventos a los botones
-quantityInputs.forEach(function(quantityInput) {
-  const decreaseButton = quantityInput.previousElementSibling;
-  const increaseButton = quantityInput.nextElementSibling;
+  // Iterar sobre los elementos y agregar los eventos a los botones
+  quantityInputs.forEach(function(quantityInput) {
+    const decreaseButton = quantityInput.previousElementSibling;
+    const increaseButton = quantityInput.nextElementSibling;
 
-  function decreaseQuantity() {
-    let currentValue = parseInt(quantityInput.value);
-    if (currentValue > 1) {
-      quantityInput.value = currentValue - 1;
+    function decreaseQuantity() {
+      currentValue = parseInt(quantityInput.value);
+      if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+      }
     }
-  }
 
-  function increaseQuantity() {
-    currentValue = parseInt(quantityInput.value);
-    if (currentValue < 10) {
-      quantityInput.value = currentValue + 1;
+    function increaseQuantity() {
+      currentValue = parseInt(quantityInput.value);
+      if (currentValue < 10) {
+        quantityInput.value = currentValue + 1;
+      }
     }
-  }
 
-  function getValue() {
-    let currentValue = parseInt(quantityInput.value);
-    console.log('Valor actual:', currentValue);
-    // Haga algo con el valor actual aquí
-  }
+    function getValue() {
+      currentValue = parseInt(quantityInput.value);
+      console.log('Valor actual:', currentValue);
+      // Haga algo con el valor actual aquíe
+    }
 
-  decreaseButton.addEventListener('click', decreaseQuantity);
-  increaseButton.addEventListener('click', increaseQuantity);
-  quantityInput.addEventListener('change', getValue);
+    decreaseButton.addEventListener('click', decreaseQuantity);
+    increaseButton.addEventListener('click', increaseQuantity);
+    quantityInput.addEventListener('change', getValue);
+  });
 });
-
-});
-
