fa30b4aacf0ef6b3a637edce7bbff0bede95b991
Nicolas Llanos <nllanos@applica.dev>
2023-03-09 12:22:55 -0300

    advances

--- assets/js/funcionesParaLaOrden.js
+++ assets/js/funcionesParaLaOrden.js
@@ -39,3 +39,34 @@ function enviarCarrito() {
 function irAPagina(pagina) {
   window.location.href = pagina;
 }
+
+window.addEventListener('load', function() {
+  const decreaseButton = document.querySelector('.decrease-button');
+  const increaseButton = document.querySelector('.increase-button');
+  const quantityInput = document.querySelector('.quantity-input');
+
+  function decreaseQuantity() {
+    let currentValue = parseInt(quantityInput.value);
+    if (currentValue > 1) {
+      quantityInput.value = currentValue - 1;
+    }
+  }
+
+  function increaseQuantity() {
+    let currentValue = parseInt(quantityInput.value);
+    if (currentValue < 10) {
+      quantityInput.value = currentValue + 1;
+    }
+  }
+
+  function getValue() {
+    let currentValue = parseInt(quantityInput.value);
+    console.log('Valor actual:', currentValue);
+    // Haga algo con el valor actual aquí
+  }
+
+  decreaseButton.addEventListener('click', decreaseQuantity);
+  increaseButton.addEventListener('click', increaseQuantity);
+  quantityInput.addEventListener('change', getValue);
+});
+
