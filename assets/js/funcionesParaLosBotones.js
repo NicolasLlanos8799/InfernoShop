9051f4661ebdd5688e69a99984f05e3b6d72dce8
Nicolas Llanos <nllanos@applica.dev>
2023-09-18 19:36:48 -0300

    advances

--- assets/js/funcionesParaLosBotones.js
+++ assets/js/funcionesParaLosBotones.js
@@ -4,16 +4,33 @@ const riquiContent = document.getElementById("riquiContent");
 const loyaltyContent = document.getElementById("loyaltyContent");
 const allproducts = document.querySelectorAll(".allproducts");
 
-document.addEventListener("DOMContentLoaded", function() {
-    loyaltyContent.style.display = "none";
+document.addEventListener("DOMContentLoaded", function () {
+    // Esperar 0.1 segundos (100 milisegundos) antes de ejecutar la función riquiButton
+    setTimeout(function () {
+        hiddenAll();
+    }, 100);
 });
 
-riquiButton.addEventListener("click", function() {
+function hiddenAll() {
+    riquiContent.style.display = "none"; // Muestra el contenido de riquiButton
+    loyaltyContent.style.display = "none"; // Oculta el contenido de loyaltyButton
+    dataContent.style.display = "none";
+}
+
+function riquiButtonAction() {
     riquiContent.style.display = "block"; // Muestra el contenido de riquiButton
     loyaltyContent.style.display = "none"; // Oculta el contenido de loyaltyButton
-});
+    dataContent.style.display = "block";
+    document.getElementById("labelMarcas").textContent = "RIQUI GENETICS"; // Cambia el texto del elemento con ID "labelMarcas" a "RIQUI GENETICS"
+}
 
-loyaltyButton.addEventListener("click", function() {
+function loyaltyButtonAction() {
     loyaltyContent.style.display = "block"; // Muestra el contenido de loyaltyButton
     riquiContent.style.display = "none"; // Oculta el contenido de riquiButton
-});
+    dataContent.style.display = "block";
+    document.getElementById("labelMarcas").textContent = "LOYALTY GENETICS"; // Cambia el texto del elemento con ID "labelMarcas" a "Nuevo Texto"
+}
+
+riquiButton.addEventListener("click", riquiButtonAction);
+
+loyaltyButton.addEventListener("click", loyaltyButtonAction); 
