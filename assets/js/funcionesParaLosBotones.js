f494bf1026b54a2bff92f173ac060fb178e02854
Nicolas Llanos <nllanos@applica.dev>
2023-09-17 23:34:33 -0300

    subiendo todos los productos de loyalty

--- assets/js/funcionesParaLosBotones.js
+++ assets/js/funcionesParaLosBotones.js
@@ -2,6 +2,11 @@ const riquiButton = document.getElementById("riquiButton");
 const loyaltyButton = document.getElementById("loyaltyButton");
 const riquiContent = document.getElementById("riquiContent");
 const loyaltyContent = document.getElementById("loyaltyContent");
+const allproducts = document.querySelectorAll(".allproducts");
+
+document.addEventListener("DOMContentLoaded", function() {
+
+});
 
 riquiButton.addEventListener("click", function() {
     riquiContent.style.display = "block"; // Muestra el contenido de riquiButton
@@ -12,4 +17,3 @@ loyaltyButton.addEventListener("click", function() {
     loyaltyContent.style.display = "block"; // Muestra el contenido de loyaltyButton
     riquiContent.style.display = "none"; // Oculta el contenido de riquiButton
 });
-
