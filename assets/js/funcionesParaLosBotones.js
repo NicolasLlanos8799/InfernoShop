68e8e86f00626b18b823843f0facc80d8f415f3b
Nicolas Llanos <nllanos@applica.dev>
2023-09-17 22:08:46 -0300

    avances, ahora se muestra y se oculta cada producto bien

---
+++ assets/js/funcionesParaLosBotones.js
@@ -0,0 +1,15 @@
+const riquiButton = document.getElementById("riquiButton");
+const loyaltyButton = document.getElementById("loyaltyButton");
+const riquiContent = document.getElementById("riquiContent");
+const loyaltyContent = document.getElementById("loyaltyContent");
+
+riquiButton.addEventListener("click", function() {
+    riquiContent.style.display = "block"; // Muestra el contenido de riquiButton
+    loyaltyContent.style.display = "none"; // Oculta el contenido de loyaltyButton
+});
+
+loyaltyButton.addEventListener("click", function() {
+    loyaltyContent.style.display = "block"; // Muestra el contenido de loyaltyButton
+    riquiContent.style.display = "none"; // Oculta el contenido de riquiButton
+});
+
